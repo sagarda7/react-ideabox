6eac198e2c1589b6c5bff866d4d659eda120f8e3
Sagar Devkota <sagarda7@yahoo.com>
2018-12-25 21:36:59 +0545

    Updates

---
+++ precache-manifest.117969110f071f9214631e3b6d5ae1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b286ef8dc234059e3a975aab9142b62",
+    "url": "/idea-box/static/media/close.1b286ef8.svg"
+  },
+  {
+    "revision": "7b1e525fb3fb2cd4c076",
+    "url": "/idea-box/static/js/runtime~main.7b1e525f.js"
+  },
+  {
+    "revision": "03ff9fd1c39fbbe23809",
+    "url": "/idea-box/static/js/main.03ff9fd1.chunk.js"
+  },
+  {
+    "revision": "4b0824c13257785f0941",
+    "url": "/idea-box/static/js/1.4b0824c1.chunk.js"
+  },
+  {
+    "revision": "03ff9fd1c39fbbe23809",
+    "url": "/idea-box/static/css/main.97cfaad3.chunk.css"
+  },
+  {
+    "revision": "4b0824c13257785f0941",
+    "url": "/idea-box/static/css/1.ba899cb4.chunk.css"
+  },
+  {
+    "revision": "81fac2a6b9eac5ebc93188c5a82b3f1a",
+    "url": "/idea-box/index.html"
+  }
+];
